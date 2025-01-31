c9880c8e36078dea6e7d6518bd18f8744e19030e
nianjindev <nianjindev@gmail.com>
2025-01-30 20:15:34 -0600

    Test

---
+++ app/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Settings from './settings';
+
+const home = () => {
+  return (
+    <>
+      <Settings />
+    </>
+  )
+}
+
+export default home;
---
+++ app/settings.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import React from "react";
+
+
+const Settings = () => {
+  return (
+    <ThemedView>
+      <ThemedText>Settings</ThemedText>
+    </ThemedView>
+  );
+};
+
+export default Settings;
