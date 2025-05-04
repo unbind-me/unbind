37d4560140ca3cf91b7b594362c2d41c445b18e9
undeemed <jerry.x0930@gmail.com>
2025-05-04 00:07:27 -0500

    task update

--- app/components/QuestList.tsx
+++ app/components/QuestList.tsx
@@ -1,4 +1,4 @@
-npimport React from "react";
+import React from "react";
 import { View, Text, StyleSheet, FlatList, Easing } from "react-native";
 import { useQuestList } from "./QuestListContext";
 import Collapsible from "react-native-collapsible";
